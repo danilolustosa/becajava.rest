ca56e1e67971509360ce31ecfa0ab279734e8b3f
Danilo Lustosa <danilolustosa@gmail.com>
2020-11-04 14:24:11 -0300

    First commit

---
+++ src/main/java/br/myapi/app/model/Usuario.java
@@ -0,0 +1,31 @@
+package br.myapi.app.model;
+
+public class Usuario {
+	
+	private String Nome;
+	private int Idade;
+	private String Endereco;
+	
+	
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public int getIdade() {
+		return Idade;
+	}
+	public void setIdade(int idade) {
+		Idade = idade;
+	}
+	public String getEndereco() {
+		return Endereco;
+	}
+	public void setEndereco(String endereco) {
+		Endereco = endereco;
+	}
+	
+	
+
+}
